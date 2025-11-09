9720ffdd8021896e490eea9837fa153acbf600ac
Arseniy <krutoi.arseniy@gmail.com>
2025-11-09 14:18:02 +0300

    feat: add cluster

---
+++ src/startCluster.ts
@@ -0,0 +1,66 @@
+import cluster from 'cluster';
+import process from 'process';
+import http from 'http';
+import os from 'os';
+
+import { createServer } from './index';
+
+const port = Number(process.env.PORT) || 3000;
+let start = 0;
+const workers: number[] = [];
+
+function startCluster() {
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+
+    for (let i = 0; i < os.cpus().length; i++) {
+      const newPort = port + i + 1;
+      workers.push(newPort);
+      cluster.fork({ PORT: newPort });
+
+      // console.log(worker);
+      // worker.send(newPort);
+    }
+
+    const balanceWorker = http.createServer((req, res) => {
+      const server = workers[start];
+      start = (start + 1) % workers.length;
+      console.log('!!!!!');
+
+      const proxy = http.request(
+        {
+          hostname: 'localhost',
+          port: server,
+          path: req.url,
+          method: req.method,
+          headers: req.headers,
+        },
+        (proxyRes) => {
+          res.writeHead(proxyRes.statusCode || 0, proxyRes.headers);
+          proxyRes.pipe(res);
+        },
+      );
+
+      proxy.on('error', (err) => {
+        console.log(`Error ${err.message}`);
+        // req.statusCode = 500;
+        res.end();
+      });
+
+      req.pipe(proxy);
+    });
+
+    balanceWorker.listen(port, () => {
+      console.log(`balanceWorker runing ${port}`);
+    });
+
+    cluster.on('exit', (worker) => {
+      console.log(`worker ${worker.process.pid} died`);
+      cluster.fork({ PORT: port + start });
+    });
+  } else {
+    createServer(Number(process.env.PORT));
+  }
+}
+
+startCluster();
